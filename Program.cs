8cc8c63c1b02038d7fc91d6dbdd4d66d9a132da0
Artur096 <artur.krasilnikov.06@bk.ru>
2026-02-19 11:06:24 +0300

    fix(program): add UTF-8 encoding for Russian language support

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ShoeStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== ОБУВНОЙ МАГАЗИН 'СТЕППЕР' ===\n");
+
+            StoreMenu menu = new StoreMenu();
+            menu.ShowMainMenu();
+
+            Console.WriteLine("\nДо новых встреч в мире стильной обуви!");
+            Console.ReadKey();
+        }
+    }
+}
